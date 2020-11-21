a895bb14195a8b693cc2a21da6202a364d767a49
Jay <ckj9014@gmail.com>
2020-11-22 08:10:25 +0900

    [apps] [users, api] User 모델 추가

--- opgc/apps/users/models.py
+++ opgc/apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class CustomModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class User(CustomModel):
+    username = models.CharField(unique=True, max_length=200, null=False) # Github ID
+    profile_image = models.CharField(max_length=500, null=True) # Github Profile Image URL
