4cc3a5a9d1af1f75237b1f75841be091227ad748
Jay <ckj9014@gmail.com>
2020-11-22 08:18:08 +0900

    [apps] [users] CustomModel 추상화

--- opgc/apps/users/models.py
+++ opgc/apps/users/models.py
@@ -5,6 +5,9 @@ class CustomModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
+
 
 class User(CustomModel):
     username = models.CharField(unique=True, max_length=200, null=False) # Github ID
