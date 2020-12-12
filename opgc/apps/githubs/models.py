c0b770e027309c0816434567d891724987272644
jay <ckj9014@gmail.com>
2020-12-12 17:14:56 +0900

    [models] [githubs] 필드명 수정 or 테이블 추가

--- opgc/apps/githubs/models.py
+++ opgc/apps/githubs/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 
 from apps.helpers.models import CustomBaseModel
-from apps.users.models import User
-
-"""
-시즌(년도)마다 github information 모델 생성
-"""
 
 
-class GithubUserInformation(CustomBaseModel):
+class GithubUser(CustomBaseModel):
     UNRANK, BRONZE, SILVER, GOLD = 0, 5, 10, 15  # 임시
     GITHUB_RANK_CHOICES = (
         (GOLD, 'Gold'),
@@ -17,9 +12,25 @@ class GithubUserInformation(CustomBaseModel):
         (UNRANK, 'unrank')
     )
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    username = models.CharField(unique=True, max_length=200, null=False) # Github ID
+    profile_image = models.CharField(max_length=500, null=True)  # Github Profile Image URL
     total_contribution = models.IntegerField(verbose_name='contribution', default=0, db_index=True)
     rank = models.SmallIntegerField(choices=GITHUB_RANK_CHOICES, default=UNRANK, blank=False)
+    company = models.CharField(max_length=100, default='', blank=True)
+    bio = models.CharField(max_length=200, default='', blank=True) # 설명
+    blog = models.CharField(max_length=100, default='', blank=True)
+    public_repos = models.IntegerField(default=0, blank=True)
+    followers = models.IntegerField(default=0, blank=True)
+    following = models.IntegerField(default=0, blank=True)
+
+
+class Repository(CustomBaseModel):
+    github_user = models.ForeignKey(GithubUser, on_delete=models.CASCADE, related_name='user')
+    contribution = models.IntegerField(verbose_name='contribution', default=0, db_index=True)
+    name = models.CharField(max_length=100, blank=False)
+    full_name = models.CharField(max_length=100, blank=False)
+    owner = models.CharField(max_length=100, blank=False)
+    organization = models.CharField(max_length=100, blank=False)
 
 
 class Language(CustomBaseModel):
@@ -28,7 +39,7 @@ class Language(CustomBaseModel):
 
 class GithubLanguage(CustomBaseModel):
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
-    github_user = models.ForeignKey(GithubUserInformation, on_delete=models.CASCADE)
+    github_user = models.ForeignKey(GithubUser, on_delete=models.CASCADE)
     contribution = models.IntegerField(verbose_name='count', default=0)
 
 
