893437f5267b1d4986afdbf6066e30c98df64525
jay <ckj9014@gmail.com>
2020-12-14 04:55:32 +0900

    [api] [githubs] organization api 추가 (좀 더 디벨롭 해야함)

--- opgc/api/githubs/urls.py
+++ opgc/api/githubs/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from api.githubs.views import GithubUserViewSet
+from api.githubs.views import GithubUserViewSet, OrganizationViewSet
 
 app_name = 'githubs'
 
@@ -8,6 +8,11 @@ github_user_list = GithubUserViewSet.as_view({
     'get': 'list',
 })
 
+organization_list = OrganizationViewSet.as_view({
+    'get': 'list',
+})
+
 urlpatterns = [
     url(r'^users/(?P<username>[-\w]+)/$', github_user_list, name='github_user_list'),
+    url(r'^users/(?P<user_pk>\d+)/organizations/$', organization_list, name='organization_list'),
 ]
--- opgc/api/githubs/views.py
+++ opgc/api/githubs/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets, mixins, exceptions
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from api.githubs.serializers import GithubUserSerializer
-from apps.githubs.models import GithubUser
+from api.githubs.serializers import GithubUserSerializer, OrganizationSerializer
+from apps.githubs.models import GithubUser, Organization, UserOrganization
 
 
 class GithubUserViewSet(viewsets.ViewSet,
@@ -18,15 +19,41 @@ class GithubUserViewSet(viewsets.ViewSet,
 
     def get_queryset(self):
         username = self.kwargs.get(self.lookup_url_kwarg)
-        github_user = GithubUser.objects.filter(username=username).get()
+
+        try:
+            github_user = GithubUser.objects.filter(username=username).get()
+        except GithubUser.DoesNotExist:
+            raise exceptions.NotFound
+
         return github_user
 
     def list(self, request, *args, **kwargs):
-        try:
-            queryset = self.get_queryset()
-        except GithubUser.DoesNotExist:
-            # todo: user가 없는 경우 생성하도록 처리하고 client에는 잠시기다리라는 안내?하기
-            raise exceptions.NotFound
-
+        # todo: user가 없는 경우 생성하도록 처리하고 client에는 잠시기다리라는 안내?하기
+        queryset = self.get_queryset()
         serializer = self.serializer_class(queryset)
+
+        return Response(serializer.data)
+
+
+class OrganizationViewSet(viewsets.ViewSet,
+                          mixins.ListModelMixin,
+                          mixins.RetrieveModelMixin):
+    """
+        endpoint : githubs/users/:user_pk/organizations/
+    """
+
+    queryset = Organization.objects.all()
+    serializer_class = GithubUserSerializer
+    lookup_url_kwarg = 'user_pk'
+
+    def get_queryset(self):
+        user_pk = int(self.kwargs.get(self.lookup_url_kwarg))
+        organizations = Organization.objects.filter(org__github_user_id=user_pk)
+
+        return organizations
+
+    def list(self, request, *args, **kwargs):
+        organizations = self.get_queryset()
+        serializer = OrganizationSerializer(organizations, many=True)
+
         return Response(serializer.data)
