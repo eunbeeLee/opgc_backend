d49629f0f7e841ffcd5a000577561dd6da62b066
jay <ckj9014@gmail.com>
2020-12-20 09:39:49 +0900

    SECRET_KEY 변경

--- opgc/conf/settings/prod.py
+++ opgc/conf/settings/prod.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'OPGC_SECRET_KEY'
+SECRET_KEY = '_OPGC_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
