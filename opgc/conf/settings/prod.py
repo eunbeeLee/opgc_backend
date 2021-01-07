315fce419f8360d8c4e42ed88313a1606d83a4ae
jay <ckj9014@gmail.com>
2021-01-07 23:15:33 +0900

    cors미들웨어 위치변경

--- opgc/conf/settings/prod.py
+++ opgc/conf/settings/prod.py
@@ -16,12 +16,12 @@ ALLOWED_HOSTS = ['OPGC_ALLOWED_HOSTS']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'conf.urls.api'
