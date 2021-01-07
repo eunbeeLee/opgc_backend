d8129fc0dc18e765c741dc0381757072be9e0c18
jay <ckj9014@gmail.com>
2021-01-07 21:23:07 +0900

    [api] [githubs] Users 호출시 org, repo 데이터도 함께 전달

--- opgc/api/githubs/serializers.py
+++ opgc/api/githubs/serializers.py
@@ -4,18 +4,32 @@ from apps.githubs.models import GithubUser, Organization, Repository
 
 
 class GithubUserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = GithubUser
         fields = '__all__'  # 모든 필드 포함
 
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+
+        org_qs = Organization.objects.filter(org__github_user_id=instance.id)
+        ret['organizations'] = OrganizationSerializer(org_qs, many=True).data
+
+        repo_qs = Repository.objects.filter(github_user_id=instance.id)
+        ret['repositories'] = RepositorySerializer(repo_qs, many=True).data
+
+        return ret
+
 
 class OrganizationSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Organization
         fields = '__all__'  # 모든 필드 포함
 
 
 class RepositorySerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Repository
         fields = '__all__'  # 모든 필드 포함
