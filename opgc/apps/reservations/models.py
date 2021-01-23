67cc3aa7204a90b99859d993915a15c2c3029e58
jay <ckj9014@gmail.com>
2021-01-24 01:07:06 +0900

    [apps] [reservations] UpdateUserQueue() 테이블명 변경, 상태 추가

--- opgc/apps/reservations/models.py
+++ opgc/apps/reservations/models.py
@@ -4,15 +4,17 @@ from apps.helpers.models import CustomBaseModel
 
 
 class UpdateUserQueue(CustomBaseModel):
-    READY, FAIL, PROGRESSING = 0, 5, 10
+    READY, FAIL, PROGRESSING, SUCCESS = 0, 5, 10, 15
     UPDATING_STATUS = (
         (READY, 'ready'),
         (FAIL, 'fail'),
-        (PROGRESSING, 'progressing')
+        (PROGRESSING, 'progressing'),
+        (SUCCESS, 'success'),
     )
 
     username = models.CharField(max_length=200, null=False)
     status = models.SmallIntegerField(choices=UPDATING_STATUS, default=READY, blank=False)
 
     class Meta:
+        db_table = 'reservations_update_user_queue'
         verbose_name = 'update_user_queue'
