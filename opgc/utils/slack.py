1aa4ffe1255f2f05d50ea189b35c516c1d1fe1b4
jay <ckj9014@gmail.com>
2021-01-24 01:08:08 +0900

    [utils] [slack] 슬랙 알림 함수 추가(채널 추가)

--- opgc/utils/slack.py
+++ opgc/utils/slack.py
@@ -36,3 +36,66 @@ def slack_notify_new_user(user: GithubUser, join_type: str = 'Dirty Boyz'):
     if channel:
         slack = slackweb.Slack(url=channel)
         slack.notify(attachments=attachments)
+
+
+def slack_notify_update_user_queue(username: str):
+    """
+        Queue 등록 알림
+    """
+    channel = settings.SLACK_CHANNEL_CRONTAB
+    server = 'PROD' if settings.IS_PROD else 'LOCAL'
+    attachments = [
+        {
+            "color": "#ff0000",
+            "title": 'RATE LIMIT 제한으로 update 실패',
+            "pretext": f'[{server}] {username}이 Queue(DB)에 등록되었습니다.',
+        }
+    ]
+
+    if channel:
+        slack = slackweb.Slack(url=channel)
+        slack.notify(attachments=attachments)
+
+
+def slack_notify_update_fail(message: str):
+    channel = settings.SLACK_CHANNEL_CRONTAB
+    server = 'PROD' if settings.IS_PROD else 'LOCAL'
+    attachments = [
+        {
+            "color": "#ff0000",
+            "title": '업데이트 실패',
+            "pretext": f'[{server}] {message}',
+        }
+    ]
+
+    if channel:
+        slack = slackweb.Slack(url=channel)
+        slack.notify(attachments=attachments)
+
+
+def slack_update_github_user(status: str, message: str, update_user=None):
+    channel = settings.SLACK_CHANNEL_CRONTAB
+    server = 'PROD' if settings.IS_PROD else 'LOCAL'
+    fields = []
+
+    if update_user:
+        fields.append({
+            "title": "총 업데이트 유저",
+            "value": f'{update_user} 명',
+            "short": True
+        })
+
+    attachments = [
+        {
+            "color": "#36a64f",
+            "title": f'예약된 깃헙 유저 정보 업데이트 {status}',
+            "fields": fields,
+        }
+    ]
+
+    if message:
+        attachments[0]['pretext'] = f'[{server}] {message}'
+
+    if channel:
+        slack = slackweb.Slack(url=channel)
+        slack.notify(attachments=attachments)
