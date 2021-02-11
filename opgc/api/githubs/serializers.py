059f5969d236cf343557896f8d98548a19093e4b
jay <ckj9014@gmail.com>
2021-02-11 21:54:25 +0900

    [api] [githubs] language 데이터 reponse 수정, status display방식 수정

--- opgc/api/githubs/serializers.py
+++ opgc/api/githubs/serializers.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework import serializers
 
 from apps.githubs.models import GithubUser, Organization, Repository, UserLanguage
@@ -16,6 +18,7 @@ class GithubUserSerializer(serializers.ModelSerializer):
         ret['repositories'] = RepositorySerializer(instance.repository.all(), many=True).data
         user_language = UserLanguage.objects.filter(github_user_id=instance.id).prefetch_related('language')
         ret['languages'] = UserLanguageSerializer(user_language, many=True).data
+        ret['status'] = instance.get_status_display()
 
         return ret
 
@@ -31,7 +34,17 @@ class RepositorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Repository
-        fields = ('id', 'contribution', 'name', 'full_name', 'owner', 'organization', 'language',)
+        fields = ('id', 'contribution', 'name', 'full_name', 'owner',
+                  'organization', 'rep_language', 'languages')
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        if ret['languages']:
+            ret['languages'] = json.loads(ret['languages'])
+        else:
+            ret['languages'] = None
+
+        return ret
 
 
 class UserLanguageSerializer(serializers.ModelSerializer):
