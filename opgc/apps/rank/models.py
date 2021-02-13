ef959d040d3e59e39063c835b16e53462db63d18
jay <ckj9014@gmail.com>
2021-02-14 02:10:13 +0900

    [apps] [rank] 모델 추가

---
+++ opgc/apps/rank/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from apps.githubs.models import GithubUser
+from apps.helpers.models import CustomBaseModel
+
+
+class UserRank(CustomBaseModel):
+    type = models.CharField(max_length=200, null=False) # lank type
+    ranking = models.SmallIntegerField(default=0, blank=False) # 순위
+    github_user = models.ForeignKey(GithubUser, db_constraint=False, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = 'rank'
+        
