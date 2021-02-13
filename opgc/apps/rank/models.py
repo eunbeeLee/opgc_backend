a484331b31329b9c336b3761b84696167c28d3e0
jay <ckj9014@gmail.com>
2021-02-14 02:59:30 +0900

    [rank] [models] score 필드 추가, verbose_name 수정

--- opgc/apps/rank/models.py
+++ opgc/apps/rank/models.py
@@ -7,8 +7,9 @@ from apps.helpers.models import CustomBaseModel
 class UserRank(CustomBaseModel):
     type = models.CharField(max_length=200, null=False) # lank type
     ranking = models.SmallIntegerField(default=0, blank=False) # 순위
+    score = models.IntegerField(default=0, blank=False) # 스코어
     github_user = models.ForeignKey(GithubUser, db_constraint=False, on_delete=models.CASCADE, null=True)
 
     class Meta:
-        verbose_name = 'rank'
+        verbose_name = 'user_rank'
         
