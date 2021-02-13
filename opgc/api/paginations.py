28827447c01f8fe9b5916ce581f610ea48e632e3
jay <ckj9014@gmail.com>
2021-02-14 02:10:55 +0900

    pagination 커스텀 (link 제거하고 cursor만 전달)

--- opgc/api/paginations.py
+++ opgc/api/paginations.py
@@ -1,7 +1,27 @@
 from rest_framework.pagination import CursorPagination
+from rest_framework.response import Response
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
 
 
 class IdOrderingPagination(CursorPagination):
     page_size = 10
     max_page_size = 1000
     ordering = 'id'
+
+    def get_paginated_response(self, data) -> Response:
+        return Response({
+            'next': self.get_cursor(self.get_next_link()),
+            'previous': self.get_cursor(self.get_previous_link()),
+            'results': data
+        })
+
+    @staticmethod
+    def get_cursor(cursor_link: str):
+        parsed = urlparse.urlparse(cursor_link)
+        cursor = parse_qs(parsed.query).get('cursor')
+
+        if cursor:
+            return cursor[0]
+        else:
+            return None
