86db93dec71787cb1e4ccea087ab98b53ea11e81
jay <ckj9014@gmail.com>
2021-03-01 15:52:21 +0900

    릴리즈 버전 변경 (2021.3.1)

--- opgc/conf/settings/prod.py
+++ opgc/conf/settings/prod.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from conf.settings.base import *
 
 # 배포 버전
-RELEASE_VERSION = '2021.2.18'
+RELEASE_VERSION = '2021.3.1'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
