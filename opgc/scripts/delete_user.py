5c66127fc64b6a159277991ccbc993219bdb1781
jay <ckj9014@gmail.com>
2021-04-16 23:44:15 +0900

    [scritps] 유저삭제시 ranking, queue 모델에서도 제거

--- opgc/scripts/delete_user.py
+++ opgc/scripts/delete_user.py
@@ -1,13 +1,17 @@
 from django.db import transaction
 
 from apps.githubs.models import GithubUser, UserLanguage, UserOrganization
+from apps.ranks.models import UserRank
+from apps.reservations.models import UpdateUserQueue
 
-USERNAME = 'usesrname'
+USERNAME = 'zzsza'
 
 
 def run():
     with transaction.atomic():
-        github_user = GithubUser.objects.filter(username=USERNAME).get().select_for_update()
+        github_user = GithubUser.objects.get(username=USERNAME)
         UserLanguage.objects.filter(github_user_id=github_user.id).delete()
         UserOrganization.objects.filter(github_user_id=github_user.id).delete()
+        UpdateUserQueue.objects.filter(username=github_user.username).delete()
+        UserRank.objects.filter(github_user_id=github_user.id).update(github_user=None)
         github_user.delete()
