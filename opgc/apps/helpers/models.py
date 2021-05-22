3ddec77cb38b4580996c3b066850d7dbe1906f79
jay <jay@dano.me>
2021-05-23 04:41:51 +0900

    [githubs] [models] 티어 choices 수정

--- opgc/apps/helpers/models.py
+++ opgc/apps/helpers/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 
@@ -7,3 +9,10 @@ class CustomBaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+    def save(self, *args, **kwargs):
+        """ On save, update timestamps """
+        if not self.id:
+            self.reg_time = datetime.now()
+        self.mod_time = datetime.now()
+        return super().save(*args, **kwargs)
