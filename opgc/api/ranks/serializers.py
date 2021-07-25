8319011acf9ef5f28eed505255c3d2eacde67983
jay <jay@dano.me>
2021-07-26 04:14:12 +0900

    [cores] 종합 순위 비율 조정

--- opgc/api/ranks/serializers.py
+++ opgc/api/ranks/serializers.py
@@ -27,8 +27,9 @@ class RankSerializer(serializers.ModelSerializer):
 
 class TierSerializer(serializers.ModelSerializer):
     tier = serializers.CharField(source='get_tier_display')
+    continuous_commit_day = serializers.IntegerField(source='total_score')  # todo: 프론트 api 변경전 임시로
 
     class Meta:
         model = GithubUser
         fields = ('id', 'username', 'name', 'avatar_url', 'tier', 'user_rank', 'company', 'bio',
-                  'continuous_commit_day')
+                  'continuous_commit_day', 'total_score', 'following', 'followers', 'total_contribution',)
