01a82cba53eb3486435492986d03ea93e5f09f94
jay <jay@dano.me>
2021-07-25 19:28:27 +0900

    [api] [notices] 공지사항 api 추가

---
+++ opgc/api/notices/views.py
@@ -0,0 +1,15 @@
+from rest_framework import mixins, viewsets
+
+from api.notices.serializers import NoticeSerializer
+from api.paginations import IdOrderingPagination
+from apps.notices.models import Notice
+
+
+class NoticeViewSet(mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    """
+    endpoint : notice/
+    """
+    queryset = Notice.objects.all()
+    serializer_class = NoticeSerializer
+    pagination_class = IdOrderingPagination
