30a2081846c523dfc21f4e8f72e018a0a65568b1
jay <jay@dano.me>
2021-07-29 19:47:50 +0900

    [api] [notice] 공지사항 상세, 리스트 api 구분

--- opgc/api/notices/serializers.py
+++ opgc/api/notices/serializers.py
@@ -8,3 +8,10 @@ class NoticeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notice
         fields = '__all__'
+
+
+class NoticeListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Notice
+        fields = ['id', 'created', 'updated', 'title']
--- opgc/api/notices/views.py
+++ opgc/api/notices/views.py
@@ -1,11 +1,12 @@
 from rest_framework import mixins, viewsets
 
-from api.notices.serializers import NoticeSerializer
+from api.notices.serializers import NoticeSerializer, NoticeListSerializer
 from api.paginations import DescIdOrderingPagination
 from apps.notices.models import Notice
 
 
 class NoticeViewSet(mixins.ListModelMixin,
+                    mixins.RetrieveModelMixin,
                     viewsets.GenericViewSet):
     """
     endpoint : notice/
@@ -13,3 +14,7 @@ class NoticeViewSet(mixins.ListModelMixin,
     queryset = Notice.objects.all()
     serializer_class = NoticeSerializer
     pagination_class = DescIdOrderingPagination
+
+    def list(self, request, *args, **kwargs):
+        self.serializer_class = NoticeListSerializer
+        return super().list(request, *args, **kwargs)
