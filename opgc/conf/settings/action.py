066fd1def749b74a644018cecd56a71da0c4e06b
jay <jay@dano.me>
2021-07-31 20:14:13 +0900

    [conf] [settings] [action] static_url 추가

--- opgc/conf/settings/action.py
+++ opgc/conf/settings/action.py
@@ -106,7 +106,7 @@ LANGUAGE_CODE = 'ko-kr'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = ''
+STATIC_URL = 'static'
 STATIC_ROOT = ''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
