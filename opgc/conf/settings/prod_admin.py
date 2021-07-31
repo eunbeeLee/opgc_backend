010f847934a6cf53184c4f89ee151dfedfce9ff9
jay <jay@dano.me>
2021-07-31 20:24:00 +0900

    [conf] [settings] [prod_admin] media config 추가

--- opgc/conf/settings/prod_admin.py
+++ opgc/conf/settings/prod_admin.py
@@ -132,7 +132,8 @@ LANGUAGE_CODE = 'ko-kr'
 #########################################
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+MEDIA_ROOT = 'media/'
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
